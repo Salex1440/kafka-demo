83102c40c6de18b82b407d27f6c97d3c3f0bbbd3
salex <ShutovAlexandr@yandex.ru>
2023-06-23 16:53:29 +0300

    Fix: implemented Serializer in Foo class

--- kafka-producer/src/main/java/com/salexcode/kafkaproducer/entity/Foo.java
+++ kafka-producer/src/main/java/com/salexcode/kafkaproducer/entity/Foo.java
@@ -4,13 +4,22 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
+import org.apache.kafka.common.serialization.Serializer;
+
 
 @Getter
 @Setter
 @AllArgsConstructor
 @ToString
-public class Foo {
+public class Foo implements Serializer {
 
     private String bar;
 
+    public Foo() {
+    }
+
+    @Override
+    public byte[] serialize(String s, Object o) {
+        return new byte[0];
+    }
 }
