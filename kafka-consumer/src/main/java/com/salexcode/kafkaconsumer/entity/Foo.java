9d5286f470c5e98fcacdb79eba764b560f28ab65
salex <ShutovAlexandr@yandex.ru>
2023-06-23 17:10:50 +0300

    Created kafka consumer

---
+++ kafka-consumer/src/main/java/com/salexcode/kafkaconsumer/entity/Foo.java
@@ -0,0 +1,25 @@
+package com.salexcode.kafkaconsumer.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import org.apache.kafka.common.serialization.Serializer;
+
+
+@Getter
+@Setter
+@AllArgsConstructor
+@ToString
+public class Foo implements Serializer {
+
+    private String bar;
+
+    public Foo() {
+    }
+
+    @Override
+    public byte[] serialize(String s, Object o) {
+        return new byte[0];
+    }
+}
---
+++ kafka-consumer/src/main/java/com/salexcode/kafkaconsumer/listener/KafkaListeners.java
@@ -0,0 +1,14 @@
+package com.salexcode.kafkaconsumer.listener;
+
+import com.salexcode.kafkaconsumer.entity.Foo;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
+
+@Component
+public class KafkaListeners {
+
+    @KafkaListener(topics = "fooTopic")
+    void listener(Foo data) {
+        System.out.println(data);
+    }
+}
