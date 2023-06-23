cfac453bc3301d0019be50073924127e4653e8f3
salex <ShutovAlexandr@yandex.ru>
2023-06-23 16:39:53 +0300

    Created a Kafka producer and a topic

---
+++ kafka-producer/src/main/java/com/salexcode/kafkaproducer/entity/Foo.java
@@ -0,0 +1,16 @@
+package com.salexcode.kafkaproducer.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@ToString
+public class Foo {
+
+    private String bar;
+
+}
